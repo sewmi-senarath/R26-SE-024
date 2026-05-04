c50c55855f3865e1fcd7a201202990855ddb57fa
Gunawardhana J C A <juthmini2001@gmail.com>
2026-05-04 23:34:15 +0530

    Implement the backend Api in task Service

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const connectDB = require("./src/config/db");
 const taskRoutes = require('./src/routes/caregiver/taskRoutes');
 const patientRoutes = require('./src/routes/caregiver/patientRoutes');
+const caregiverRoutes = require('./src/routes/caregiver/caregiverRoutes');
 
 connectDB();
 const app = express();
@@ -16,6 +17,7 @@ app.use(cors());
 // Caregiver Portal routes ...
 app.use('/api/caregiver/tasks', taskRoutes);
 app.use('/api/caregiver/patients', patientRoutes);
+app.use('/api/caregiver/profile', caregiverRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
